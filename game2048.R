b15b06d9cfea8a596d7e9ad8a466099f8a9a15db
chiemvs <chiemvs@gmail.com>
2018-05-20 14:15:43 +0200

    added global scoring and ratio for 2 to 4

--- game2048.R
+++ game2048.R
@@ -1,13 +1,13 @@
 # Test script for the game 2048
 
-# To-do: add 2:4 ratio for the new tiles
+# added a 90:10 ratio for 2:4 in the new tile creation
 
 n_col <- 4
 n_row <- 4
 
 board_t0 <- matrix(data = NA, nrow = n_row, ncol = n_col)
 
-# This function adds a tile to the board, on a random free spot. It puts either the value 2 or the value 4
+# This function adds a tile to the board, on a random free spot. It puts the value 2 in 90 percent of the times and 4 in the remaining 10 percent.
 add_tile <- function(board) {
  
   # is.na returns True when free spot, False when occupied with a tile, returns a matrix of similar dimensions.
@@ -21,7 +21,7 @@ add_tile <- function(board) {
   random_spot <- free_spots[sample(x = 1:nrow(free_spots), size = 1),]
   new_row <- random_spot['row']
   new_col <- random_spot['col']
-  new_value <- sample(x = c(2,4), size = 1) # Currently a 50/50 ratio
+  new_value <- sample(x = c(rep(x = 2, 9), rep(x = 4, 1)), size = 1) # Gives a 90/10 ratio.
   
   # Place onto the board
   board[new_row, new_col] <- new_value
@@ -39,8 +39,9 @@ clockwise_rotate <- function(board, n_times) {
 }
 
 # This function moves all the tiles as far as possible in the direction of movement. 
+# Then it merges the tiles and adds the number to the score
 # To unify the procedure it rotates the board a designated number of times so the direction is always upward.
-move_tiles <- function(board, direction) {
+move_tiles <- function(board, direction, score) {
  
   # list with possible clockwise rotations of the matrix, check whether the supplied direction is in it.
   rotations <- list(up = 0, left = 1, down = 2, right = 3) 
@@ -56,40 +57,22 @@ move_tiles <- function(board, direction) {
     
     col_clean <- col[!is.na(col)] # removes all NA
     
-    # Procedure for the pairwise doubling.
+    # Procedure for the pairwise doubling and scoring of the doubled values.
     run_lengths <- rle(col_clean) # Stores in order: each value in the column ($values) and in how many consecutive! tiles it occurs ($lengths)
     col_merged <- lapply(X = seq_along(run_lengths$values), FUN = function(index) {
       doubles <- rep(x = 2 * run_lengths$values[[index]], times = floor(run_lengths$lengths[[index]] / 2)) # paste the double of the value for each pair in the consecutive sequence, starting at the beginning.
       single <- rep(x = run_lengths$values[[index]], times = run_lengths$lengths[[index]] %% 2) # take account of the possible single value that remains. This one is not doubled. Modulo two (%% 2) becomes zero if the run length is a multiple of two and there are only pairs.
+      assign(x = "score", value = score + sum(doubles), envir = parent.frame(4)) # Adds the doubled values to the score, which is defined four environments above this one.
       return(c(doubles,single))
     })
-    col_merged <- do.call(what = c, col_merged) # Concatenate the list output (vectors of various lengths) into a vector
-    
+    col_merged <- do.call(what = c, args = col_merged) # Concatenate the values in the list of list output (vectors of various lengths) into a vector
+      
     # Fill the column up with NA's till it is the right length again
     col_full <- c(col_merged, rep(NA, length.out = length(col) - length(col_merged))) 
   }))
   
   # Bind the columns and do the remainder of rotations for a total of 4, so the board is in original orientation again.
   board <- clockwise_rotate(board = do.call(cbind, columns_new), n_times = 4 - n_rot)
-  return(board)
+  return(list(board = board, score = score))
 }
 
-# Function to play the game interactively
-start_2048 <- function() {
-  
-  print("Welcome to 2048, type 'end' to stop the game")
-  board <- board_t0
-  input <- "up" # just an initial value so it does not crash.
-  
-  while(input != "end") {
-    
-    board <- move_tiles(board = board, direction = input)
-    print(board)
-    board <- add_tile(board = board)
-    print(board)
-    input <- readline(prompt = "enter direction: ")
-  }
-  stop()
-}
-
-start_2048()
